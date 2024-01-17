d22328822215022ee4e9f6738bf75125bcb17d93
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-17 06:14:57 +0300

    create project

---
+++ src/main/java/health/d_health_api/DHealthApiApplication.java
@@ -0,0 +1,13 @@
+package health.d_health_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DHealthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DHealthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/health/d_health_api/DHealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package health.d_health_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DHealthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
