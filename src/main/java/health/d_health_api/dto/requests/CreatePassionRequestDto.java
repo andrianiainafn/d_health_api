999d593cf734e5f701e6d96187dfe52ed5da00eb
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-18 22:53:18 +0300

    feature: create dto for the create passion request

---
+++ src/main/java/health/d_health_api/dto/requests/CreatePassionRequestDto.java
@@ -0,0 +1,10 @@
+package health.d_health_api.dto.requests;
+
+import java.util.Date;
+
+public class CreatePassionRequestDto {
+    private String username;
+    private String email;
+    private String password;
+    private Date dateOfBirth;
+}
