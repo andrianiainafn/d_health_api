120509d497f35340e8e9b519f268e681c6efbbfb
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-18 22:51:30 +0300

    feature: create passion service implementation

---
+++ src/main/java/health/d_health_api/serviceImpls/PassionServiceImpl.java
@@ -0,0 +1,11 @@
+package health.d_health_api.serviceImpls;
+
+import health.d_health_api.dto.requests.CreatePassionRequestDto;
+import health.d_health_api.services.PassionService;
+
+public class PassionServiceImpl implements PassionService {
+    @Override
+    public void createPassion(CreatePassionRequestDto createPassionRequestDto) {
+
+    }
+}
