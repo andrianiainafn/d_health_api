35a8c24ca89a6a9bb24e422f72e9cdf6bf649224
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-21 12:15:48 +0300

    refactor : remove create passion methode from here

--- src/main/java/health/d_health_api/services/PassionService.java
+++ src/main/java/health/d_health_api/services/PassionService.java
@@ -4,5 +4,5 @@ package health.d_health_api.services;
 import health.d_health_api.dto.requests.CreatePassionRequestDto;
 
 public interface PassionService {
-    public void createPassion(CreatePassionRequestDto createPassionRequestDto);
+
 }
