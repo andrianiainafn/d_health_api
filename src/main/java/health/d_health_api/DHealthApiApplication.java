b4dbc12c827c2bf4323e4bc0c544e05ccc2b3390
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-21 12:15:20 +0300

    feature : add bean bcrypt for hashing password

--- src/main/java/health/d_health_api/DHealthApiApplication.java
+++ src/main/java/health/d_health_api/DHealthApiApplication.java
@@ -1,13 +1,24 @@
 package health.d_health_api;
 
+import health.d_health_api.config.RsaKeysConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableConfigurationProperties(RsaKeysConfig.class)
 @SpringBootApplication
 public class DHealthApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DHealthApiApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
