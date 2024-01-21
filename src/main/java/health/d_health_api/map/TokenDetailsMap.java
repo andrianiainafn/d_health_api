1f48b6c89b9a1d429c2f4fbe50e87938be543e38
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-21 12:13:01 +0300

    feature : change id type to string

--- src/main/java/health/d_health_api/map/TokenDetailsMap.java
+++ src/main/java/health/d_health_api/map/TokenDetailsMap.java
@@ -1,7 +1,15 @@
 package health.d_health_api.map;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
 public class TokenDetailsMap {
     private String scope;
     private String subject;
-    private Long userId;
+    private String userId;
 }
