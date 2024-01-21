0263a3a24f4bbf6884df96f521a90d590aaf1197
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-21 12:09:58 +0300

    feature: change the return value of Register passion

--- src/main/java/health/d_health_api/services/AuthService.java
+++ src/main/java/health/d_health_api/services/AuthService.java
@@ -1,5 +1,6 @@
 package health.d_health_api.services;
 
+import health.d_health_api.dto.requests.CreatePassionRequestDto;
 import health.d_health_api.map.TokenDetailsMap;
 import org.springframework.security.oauth2.jwt.Jwt;
 
@@ -8,7 +9,9 @@ import java.time.Instant;
 public interface AuthService {
     Jwt decodeToken(String token);
     void generateScopeAndSubjectWithPasswordGrand(String email, String password, TokenDetailsMap tokenDetailsMap);
-    void generateScopeAndSubjectWithRefreshTokenGrand(String email, String password, TokenDetailsMap tokenDetailsMap);
+    void generateScopeAndSubjectWithRefreshTokenGrand(String refreshToken,String email, String password, TokenDetailsMap tokenDetailsMap);
     String generateRefreshToken(String scope, Instant instant);
     String generateToken(String scope,String subject,Instant instant,boolean withRefreshToken , Long userId);
+
+    CreatePassionRequestDto registerPassion(CreatePassionRequestDto createPassionRequest);
 }
