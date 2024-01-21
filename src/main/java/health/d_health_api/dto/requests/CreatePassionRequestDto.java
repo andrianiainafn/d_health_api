0bddf35f35f499fbd1509eb33231904ec71f0e02
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-01-21 12:14:38 +0300

    feature : add some util annotation

--- src/main/java/health/d_health_api/dto/requests/CreatePassionRequestDto.java
+++ src/main/java/health/d_health_api/dto/requests/CreatePassionRequestDto.java
@@ -1,10 +1,29 @@
 package health.d_health_api.dto.requests;
 
+import health.d_health_api.model.Passion;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CreatePassionRequestDto {
     private String username;
     private String email;
     private String password;
     private Date dateOfBirth;
+
+    public static CreatePassionRequestDto fromPassion(Passion passion){
+        return CreatePassionRequestDto.builder()
+                .password(passion.getPassword())
+                .email(passion.getEmail())
+                .dateOfBirth(passion.getDateOfBirth())
+                .username(passion.getUsername())
+                .build();
+    }
 }
