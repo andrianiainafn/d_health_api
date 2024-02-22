dbe9496c1fce1277f2e104b799f84b278e03dc87
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-22 15:24:23 +0300

    feature : add load by email methode

--- src/main/java/health/d_health_api/repositories/PassionRepository.java
+++ src/main/java/health/d_health_api/repositories/PassionRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface PassionRepository extends JpaRepository<Passion,Long> {
-
+    Passion findPassionByEmail(String email);
 }
