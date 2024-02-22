0419f871317a606b2a82df2b611b94140aa38c82
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-02-22 15:23:57 +0300

    feature : add load by email service

--- src/main/java/health/d_health_api/serviceImpls/PassionServiceImpl.java
+++ src/main/java/health/d_health_api/serviceImpls/PassionServiceImpl.java
@@ -1,8 +1,23 @@
 package health.d_health_api.serviceImpls;
 
-import health.d_health_api.dto.requests.CreatePassionRequestDto;
+
+import health.d_health_api.model.Passion;
+import health.d_health_api.repositories.PassionRepository;
 import health.d_health_api.services.PassionService;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class PassionServiceImpl implements PassionService {
+    private final PassionRepository passionRepository;
 
+    public PassionServiceImpl(PassionRepository passionRepository) {
+        this.passionRepository = passionRepository;
+    }
+
+    @Override
+    public Passion loadPassionByEmail(String email) {
+        return passionRepository.findPassionByEmail(email);
+    }
 }
--- src/main/java/health/d_health_api/services/PassionService.java
+++ src/main/java/health/d_health_api/services/PassionService.java
@@ -2,7 +2,8 @@ package health.d_health_api.services;
 
 
 import health.d_health_api.dto.requests.CreatePassionRequestDto;
+import health.d_health_api.model.Passion;
 
 public interface PassionService {
-
+    Passion loadPassionByEmail(String email);
 }
