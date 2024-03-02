e5d4b4a4c6aa3c0252d902230aed5ee6d8f26910
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-02 22:49:10 +0300

    feature: add refresh token to passion auth response dto

--- src/main/java/health/d_health_api/dto/responses/AuthPassionResponseDto.java
+++ src/main/java/health/d_health_api/dto/responses/AuthPassionResponseDto.java
@@ -1,19 +1,17 @@
 package health.d_health_api.dto.responses;
 
-import health.d_health_api.model.Passion;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class CreatePassionResponseDto {
+public class AuthPassionResponseDto {
     private String username;
     private String email;
     private String token;
+    private String refreshToken;
 }
