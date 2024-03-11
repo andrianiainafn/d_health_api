853926bc3ab5a87f62d6e1346046f1d57fffa96e
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-11 21:34:18 +0300

    feat add methode to get the list of profiles

--- src/main/java/health/d_health_api/services/ProfileService.java
+++ src/main/java/health/d_health_api/services/ProfileService.java
@@ -1,9 +1,13 @@
 package health.d_health_api.services;
 
 import health.d_health_api.dto.requests.CreateProfileDto;
+import health.d_health_api.dto.responses.ProfileDto;
 import health.d_health_api.model.Profile;
 
+import java.util.List;
+
 public interface ProfileService {
-    Profile createProfile (CreateProfileDto profile, String token);
-    void getProfileDetails ();
+    void createProfile (CreateProfileDto profile, String token);
+    Profile getProfileDetails ();
+    List<ProfileDto> getProfiles ();
 }
