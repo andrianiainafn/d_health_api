520bd08e48c0c2a09fa7825ac4a0a105dc54d9ca
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-11 11:28:07 +0300

    refactor importation

--- src/main/java/health/d_health_api/controller/AuthController.java
+++ src/main/java/health/d_health_api/controller/AuthController.java
@@ -7,13 +7,9 @@ import health.d_health_api.exceptions.RessourceNotFoundException;
 import health.d_health_api.services.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
