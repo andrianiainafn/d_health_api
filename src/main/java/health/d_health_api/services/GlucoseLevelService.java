bd04903adba19c49340376288b8711947ac12636
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-11 21:41:24 +0300

    fix: add methode to list all glucose levels

--- src/main/java/health/d_health_api/services/GlucoseLevelService.java
+++ src/main/java/health/d_health_api/services/GlucoseLevelService.java
@@ -3,6 +3,9 @@ package health.d_health_api.services;
 import health.d_health_api.dto.requests.CreateGlucoseLevelRequest;
 import health.d_health_api.model.GlucoseLevel;
 
+import java.util.List;
+
 public interface GlucoseLevelService {
     GlucoseLevel createGlucoseLevel(CreateGlucoseLevelRequest glucoseLevelRequest, String profileId);
+    List<GlucoseLevel> getAllGlucoseLevels();
 }
