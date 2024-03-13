f3e1ecdbe166cd3a19d024506348e070e5330631
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-13 22:20:17 +0300

    feat: add methods for appointment service

--- src/main/java/health/d_health_api/services/AppointmentService.java
+++ src/main/java/health/d_health_api/services/AppointmentService.java
@@ -1,8 +1,12 @@
 package health.d_health_api.services;
 
 import health.d_health_api.dto.requests.AppointmentRequestDto;
+import health.d_health_api.enums.AppointmentStatus;
 import health.d_health_api.model.Appointment;
+
+import java.util.List;
 
 public interface AppointmentService {
     Appointment createAppointment(AppointmentRequestDto appointmentRequestDto,String token);
+    List<Appointment> getListOfAppointmentForPassion(String token, AppointmentStatus status);
 }
