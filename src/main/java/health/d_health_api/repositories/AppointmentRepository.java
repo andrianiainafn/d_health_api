19b71aa21529ddb7a41a1a56f6ec108d888bcff5
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-13 22:19:23 +0300

    feat: implement appointment repository

--- src/main/java/health/d_health_api/repositories/AppointmentRepository.java
+++ src/main/java/health/d_health_api/repositories/AppointmentRepository.java
@@ -1,8 +1,12 @@
 package health.d_health_api.repositories;
 
+import health.d_health_api.enums.AppointmentStatus;
 import health.d_health_api.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppointmentRepository extends JpaRepository<String, Appointment> {
+import java.util.List;
 
+public interface AppointmentRepository extends JpaRepository<Appointment, String> {
+    List<Appointment> findAppointmentByPassion_PassionId(String passionId);
+    List<Appointment> findAppointmentByAppointmentStatusAndPassion_PassionId(AppointmentStatus appointmentStatus, String passionId);
 }
