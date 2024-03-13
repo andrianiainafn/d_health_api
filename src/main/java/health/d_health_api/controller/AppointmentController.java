07eded2f9be8350e7017f2293252f3a5319f5f9d
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-03-13 22:18:53 +0300

    feat: implement appointment controller

--- src/main/java/health/d_health_api/controller/AppointmentController.java
+++ src/main/java/health/d_health_api/controller/AppointmentController.java
@@ -2,18 +2,41 @@ package health.d_health_api.controller;
 
 
 import health.d_health_api.dto.requests.AppointmentRequestDto;
+import health.d_health_api.enums.AppointmentStatus;
 import health.d_health_api.model.Appointment;
+import health.d_health_api.services.AppointmentService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/appointment")
 public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
 
     @PostMapping("")
     public ResponseEntity<Appointment> createAppointment(@RequestBody AppointmentRequestDto appointmentRequestDto,@RequestHeader (name = HttpHeaders.AUTHORIZATION) String authorizationHeader) {
-        return  new ResponseEntity<>(HttpStatus.CREATED);
+        if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            return  new ResponseEntity<>(appointmentService.createAppointment(appointmentRequestDto,token),HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Appointment>> getAppointmentForPassion(@RequestHeader (name = HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestParam AppointmentStatus status){
+        if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            return  new ResponseEntity<>(appointmentService.getListOfAppointmentForPassion(token,status),HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
